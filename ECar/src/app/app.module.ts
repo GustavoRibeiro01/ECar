5f32a855f3e3368dec74a566d0f17b7d79dc36ba
GustavoRibeiro01 <36051926+GustavoRibeiro01@users.noreply.github.com>
2019-10-07 20:32:02 -0300

    Materialize, loginComponent

--- ECar/src/app/app.module.ts
+++ ECar/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MzButtonModule, MzInputModule } from 'ngx-materialize';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MzCheckboxModule } from 'ngx-materialize'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MzButtonModule,
+    MzInputModule,
+    MzCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
